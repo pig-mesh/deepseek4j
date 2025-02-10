5bde9be0841fb26d5eef47c60f966670f41cb658
code2t <code2t@dev.org>
2025-02-10 10:37:05 +0800

    remove unused package

--- deepseek4j-example/src/main/java/io/github/pigmesh/ai/deepseek/example/ollama/OllamaLocalController.java
+++ deepseek4j-example/src/main/java/io/github/pigmesh/ai/deepseek/example/ollama/OllamaLocalController.java
@@ -3,7 +3,6 @@ package io.github.pigmesh.ai.deepseek.example.ollama;
 import io.github.pigmesh.ai.deepseek.core.DeepSeekClient;
 import io.github.pigmesh.ai.deepseek.core.chat.ChatCompletionResponse;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
