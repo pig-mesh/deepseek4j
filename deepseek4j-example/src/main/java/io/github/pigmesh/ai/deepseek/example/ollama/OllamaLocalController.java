3614cd59d2600c4f363e512da1a424c47f0da297
code2t <code2t@dev.org>
2025-02-10 10:34:07 +0800

    add deepseek4j-example project

---
+++ deepseek4j-example/src/main/java/io/github/pigmesh/ai/deepseek/example/ollama/OllamaLocalController.java
@@ -0,0 +1,26 @@
+package io.github.pigmesh.ai.deepseek.example.ollama;
+
+import io.github.pigmesh.ai.deepseek.core.DeepSeekClient;
+import io.github.pigmesh.ai.deepseek.core.chat.ChatCompletionResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping
+public class OllamaLocalController {
+
+    @Resource
+    private DeepSeekClient deepSeekClient;
+
+    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatCompletionResponse> chat(String prompt) {
+        return deepSeekClient.chatFluxCompletion(prompt);
+    }
+
+}
