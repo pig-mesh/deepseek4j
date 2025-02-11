aff858b73c7d5b9ec63dae8fc52fd5ac9da17c26
code2t <code2t@dev.org>
2025-02-11 17:00:11 +0800

    fix: update CorsConfig

--- deepseek4j-example/src/main/java/io/github/pigmesh/ai/deepseek/example/config/CorsConfig.java
+++ deepseek4j-example/src/main/java/io/github/pigmesh/ai/deepseek/example/config/CorsConfig.java
@@ -7,11 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*");
+	}
+
 }
